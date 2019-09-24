a44f71b5d36c1769a047c6fd0b0756f0f30aa1d3
teppo223 <t6hote00@students.oamk.fi>
2019-09-24 18:11:50 +0300

    Valmis

--- Lab5_1/ViewController.swift
+++ Lab5_1/ViewController.swift
@@ -10,11 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    var minutes:Int = 0
+    var seconds:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        pickerView.delegate = self
+        pickerView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.destination is SecondViewController
+        {
+            let vc = segue.destination as? SecondViewController
+            vc?.timerMinutes = minutes
+            vc?.timerSeconds = seconds
+        }
+    }
 }
 
+extension ViewController:UIPickerViewDelegate,UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0,1:
+            return 60
+            
+        default:
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return pickerView.frame.size.width/2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return "\(row) Minute"
+        case 1:
+            return "\(row) Second"
+        default:
+            return ""
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case 0:
+            minutes = row
+        case 1:
+            seconds = row
+        default:
+            break;
+        }
+    }
+}
