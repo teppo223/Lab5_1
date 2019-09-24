050d004198a2b98f83b73ba20dd8a64bf32072d4
teppo223 <t6hote00@students.oamk.fi>
2019-09-24 16:47:27 +0300

    Initial Commit

---
+++ Lab5_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab5_1
+//
+//  Created by Toni Kontio on 24/09/2019.
+//  Copyright © 2019 Toni Kontio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
